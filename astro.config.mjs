686af5137670bd479995ff0cdcc8783c6f5ea8ad
Nick <nickshevr@gmail.com>
2024-12-27 12:19:32 +0300

    Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://astronaut.github.io',
+	base: 'my-repo',
 	integrations: [mdx(), sitemap()],
 });
