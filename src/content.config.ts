da3b060e3b9a612983e9e37fe1c62331cea78b86
Nick <nickshevr@gmail.com>
2024-12-27 12:17:42 +0300

    Product Page

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,24 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
+const products = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	loader: glob({ base: './src/content/products', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
+		who: z.string(),
+		when: z.string(),
+		how: z.string(),
+		ingridients: z.string(),
 		description: z.string(),
 		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		table: z.array(z.object({
+			factor: z.string(),
+			meaning: z.string(),
+		})),
 	}),
 });
 
-export const collections = { blog };
+export const collections = { products };
