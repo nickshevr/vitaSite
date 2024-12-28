2b7d41a7a594a878bc6f6033e6b29e24cb751440
Nick <nickshevr@gmail.com>
2024-12-28 12:19:35 +0300

    Fix relative urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astronaut.github.io',
-	base: 'my-repo',
+	site: 'https://nickshevr.github.io',
+	base: 'vitaSite',
 	integrations: [mdx(), sitemap()],
 });
