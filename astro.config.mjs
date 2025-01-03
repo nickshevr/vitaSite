5d4b2487cbcd019fd0e6266e48aa64ff2547bece
Nick <nickshevr@gmail.com>
2025-01-03 19:59:00 +0300

    Add prefetch strategy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	prefetch: {
+		defaultStrategy: 'viewport'
+	},
 	site: 'https://nickshevr.github.io',
 	base: 'vitaSite',
 	integrations: [mdx(), sitemap()],
