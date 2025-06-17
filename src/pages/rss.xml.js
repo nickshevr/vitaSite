13826091b2de91e07ba015411a5c424f9b04e6ef
Nick <nickshevr@gmail.com>
2025-06-17 19:19:19 +0300

    Fix sitemap

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,7 +10,7 @@ export async function GET(context) {
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/products/${post.slug}/`,
+			link: `/products/${post.data.id}/`,
 		})),
 	});
 }
